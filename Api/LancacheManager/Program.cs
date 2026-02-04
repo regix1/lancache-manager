3dfd809b40e9b05da194c767078eeede45851676
regix1 <dateague627@gmail.com>
2026-02-04 16:56:01 -0600

    testing new service

--- Api/LancacheManager/Program.cs
+++ Api/LancacheManager/Program.cs
@@ -437,6 +437,7 @@ builder.Services.AddHostedService(provider => provider.GetRequiredService<Operat
 // Register background services
 builder.Services.AddHostedService<LiveLogMonitorService>();
 builder.Services.AddHostedService<DownloadCleanupService>();
+builder.Services.AddHostedService<DepotMappingBackfillService>();
 
 // Register RustSpeedTrackerService for real-time per-game download speed monitoring (uses Rust for faster parsing)
 builder.Services.AddSingleton<RustSpeedTrackerService>();
